41690456f9572743e06d41dd8d3a34e770647eae
seobkim <kks7824@gmail.com>
2022-06-26 23:40:05 +0900

    init: project first commit

---
+++ src/main/java/com/example/react_aws_proj/ReactAwsProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.react_aws_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAwsProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAwsProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/react_aws_proj/ReactAwsProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react_aws_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAwsProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
